35f3f9fd94f07bf239afaf6f5bef0a9b29167a9a
mcpackard415 <mcpackard@uchicago.edu>
2022-01-05 12:54:43 -0600

    Moved WOS data and scripts

--- wos_2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mysql --ssl-ca=../wos-db.uchicago.edu.ca \
-      --ssl-key=../wos-db.uchicago.edu-key.pem \
-      --ssl-cert=../wos-db.uchicago.edu-cert.pem \
-      -h wos-db.uchicago.edu --user=mcpackard wos_new < wos.sql > wos.tab
