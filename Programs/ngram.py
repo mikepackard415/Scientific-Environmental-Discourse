54c41a726b4ad07bec6ca7511b439f36d90c132e
mikepackard415 <mcpackard@uchicago.edu>
2022-03-05 12:15:18 -0600

    edit ngram

--- Programs/ngram.py
+++ Programs/ngram.py
@@ -8,8 +8,9 @@ try:
 except OSError:
     nlp = spacy.load("en_core_web_sm")
 
-#env = pd.read_csv('../Data/Environmental Discourse/env.csv', index_col=0)
-env = pd.read_pickle('../Data/Environmental-Discourse/env.pkl')
+env = pd.read_csv('../Data/Environmental-Discourse/env.csv', index_col=0)
+env = env.sample(20000, random_state=3291995)
+#env = pd.read_pickle('../Data/Environmental-Discourse/env.pkl')
 
 def word_tokenize(word_list, model=nlp, MAX_LEN=1500000):
     
@@ -66,20 +67,23 @@ def normalizeTokens(word_list, extra_stop=[], model=nlp, lemma=True, MAX_LEN=150
     return normalized
 
 # Apply tokenization and normalization functions
-#env['tokenized_text'] = env['text'].apply(lambda x: word_tokenize(x))
-#env['normalized_tokens'] = env['tokenized_text'].apply(lambda x: normalizeTokens(x, lemma=False))
-#env.to_pickle('../Data/Environmental Discourse/env.pkl')
+print("Tokenizing...")
+env['tokenized_text'] = env['text'].apply(lambda x: word_tokenize(x))
+
+print("Normalizing...")
+env['normalized_tokens'] = env['tokenized_text'].apply(lambda x: normalizeTokens(x, lemma=False))
+env.to_pickle('../Data/Environmental-Discourse/env_ngram_sample.pkl')
 
 print("Bigrams...")
 env['bigrams'] = env['normalized_tokens'].apply(lambda x: [i for i in ngrams(x, 2)])
 bigrams = pd.Series(env['bigrams'].sum()).value_counts().head(100)
 bigram_df = pd.DataFrame({'bigram': bigrams})
-bigram_df.to_csv('../Data/Environmental Discourse/bigrams.csv')
+bigram_df.to_csv('../Data/Environmental-Discourse/bigrams.csv')
 
 print("Trigrams...")
 env['trigrams'] = env['normalized_tokens'].apply(lambda x: [i for i in ngrams(x, 3)])
 trigrams = pd.Series(env['trigrams'].sum()).value_counts().head(100)
 trigram_df = pd.DataFrame({'trigram': trigrams})
-trigram_df.to_csv('../Data/Environmental Discourse/trigrams.csv')
+trigram_df.to_csv('../Data/Environmental-Discourse/trigrams.csv')
 
 print("Complete!")
