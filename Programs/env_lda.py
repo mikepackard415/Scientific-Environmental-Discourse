adaca0f6dfcdc51cc0b714448de83b391c62d735
mcpackard415 <mcpackard@uchicago.edu>
2022-03-11 11:57:29 -0600

    ntr

--- Programs/env_lda.py
+++ Programs/env_lda.py
@@ -2,29 +2,34 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from lda import LDA
 
-print("Reading data...")
-env = pd.read_csv('../Data/Environmental Discourse/env.csv', index_col=0).sample(1500, random_state=3291995)
-
 def learn_topics(texts, topicnum):
 
     # Get vocabulary and word counts.  Use the top 10,000 most frequent
     # lowercase unigrams with at least 3 alphabetical, non-numeric characters,
     # punctuation treated as separators.
     print("Vectorizing...")
-    CVzer = CountVectorizer(max_features=5000,
+    CVzer = CountVectorizer(max_features=10000,
                             lowercase=True)
     doc_vcnts = CVzer.fit_transform(texts)
     vocabulary = CVzer.get_feature_names()
 
     # Learn topics.  Refresh conrols print frequency.
     print("LDA")
-    lda_model = LDA(topicnum, n_iter=500, refresh=50) 
+    lda_model = LDA(topicnum, n_iter=4000, refresh=500) 
     doc_topic = lda_model.fit_transform(doc_vcnts)
     topic_word = lda_model.topic_word_
 
     return doc_topic, topic_word, vocabulary
 
-doc_topic, topic_word, vocabulary = learn_topics(env.text, 100)
+print("Reading data...")
+env = pd.read_csv('../Data/Environmental Discourse/env_processed.csv', index_col=0)
+env = env[~env.text_processed.isna()]
 
-print(type(doc_topic))
+doc_topic, topic_word, vocabulary = learn_topics(env.text_processed, 100)
+
 print(doc_topic[0,:])
+
+for i in range(100):
+    env['topic_{}'.format(i)] = doc_topic[:, i]
+
+env.to_csv('../Data/Environmental Discourse/env_lda.csv')
