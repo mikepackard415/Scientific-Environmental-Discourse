4ed3809bb863abada7789c37e0592b1b8cb3f3ad
mcpackard415 <mcpackard@uchicago.edu>
2022-03-15 09:06:21 -0500

    dwe

--- Programs/processing_dwe.py
+++ Programs/processing_dwe.py
@@ -63,14 +63,78 @@ def normalizeTokens(word_list, extra_stop=[], model=nlp, lemma=True, MAX_LEN=150
 
     return normalized
 
+def ngram_tagger(tokens):
+    n = len(tokens)
+    i = 0
+    tokens_q = []
+    tokens_qt = []
+    tokens_qtb = []
+    
+    # quadgrams
+    while i < n:
+        words = '_'.join(tokens[i:i+4])
+        if words in quadgrams:
+            tokens_q.append(words)
+            i += 4
+        else:
+            tokens_q.append(tokens[i])
+            i += 1
+    
+    # trigrams
+    n = len(tokens_q)
+    i = 0
+    while i < n:
+        words = '_'.join(tokens_q[i:i+3])
+        if words in trigrams:
+            tokens_qt.append(words)
+            i += 3
+        else:
+            tokens_qt.append(tokens_q[i])
+            i += 1
+    
+    # bigrams
+    n = len(tokens_qt)
+    i = 0
+    while i < n:
+        words = '_'.join(tokens_qt[i:i+2])
+        if words in bigrams:
+            tokens_qtb.append(words)
+            i += 2
+        else:
+            tokens_qtb.append(tokens_qt[i])
+            i += 1
+    
+    return tokens_qtb
+
 def sent_tokenize(word_list, model=nlp):
     doc = model(word_list)
     sentences = [sent.text.strip() for sent in doc.sents]
     return sentences
 
+quadgrams = [('intergovernmental', 'panel', 'climate', 'change'),
+             ('natural', 'resources', 'defense', 'council'),
+             ('coal', 'fired', 'power', 'plants'),
+             ('national', 'oceanic', 'atmospheric', 'administration')]
+
+tr = pd.read_csv('../Data/' + path + '/trigrams.csv', converters={'Unnamed: 0': ast.literal_eval})
+tr.columns = ['trigram', 'freq', 'tag']
+trigrams = [t for t in tr[tr.tag == 1].trigram]
+
+b = pd.read_csv('../Data/' + path + '/bigrams.csv', converters={'Unnamed: 0': ast.literal_eval})
+b.columns = ['bigram', 'freq', 'tag']
+bigrams = [t for t in b[b.tag == 1].bigram]
+
+quadgrams = ['_'.join(t) for t in quadgrams]
+trigrams = ['_'.join(t) for t in trigrams]
+bigrams = ['_'.join(t) for t in bigrams]
+
+
+
 d_env = dd.from_pandas(env, npartitions=effective_n_jobs(-1))
-d_env['tokenized_sents'] = d_env.text.map(lambda x: [word_tokenize(s) for s in sent_tokenize(x)])
-d_env['normalized_sents'] = d_env.tokenized_sents.map(lambda x: [normalizeTokens(s, lemma=False) for s in x])
+d_env['sents'] = d_env.text.map(lambda x: [ngram_tagger(
+                                           normalizeTokens(
+                                           word_tokenize(s), lemma=False)) for s in sent_tokenize(x)])
+d_env['sents'] = d_env.sents.map(lambda x: [s for s in x if len(s)>0])
 env = d_env.compute()
 
 env.to_pickle('../Data/'+path+'/env_processed_sent.pkl')
