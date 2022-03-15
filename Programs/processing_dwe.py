a9964bf11bc0f6cef6af0773ec03ad6433e1cf17
mcpackard415 <mcpackard@uchicago.edu>
2022-03-14 20:34:58 -0500

    dwe

---
+++ Programs/processing_dwe.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import dask.dataframe as dd
+from gensim.utils import effective_n_jobs
+import spacy
+try:
+    nlp = spacy.load("en")
+except OSError:
+    nlp = spacy.load("en_core_web_sm")
+
+path = 'Environmental-Discourse'
+env = pd.read_pickle('../Data/'+path+'/env_0.pkl')
+
+def word_tokenize(word_list, model=nlp, MAX_LEN=1500000):
+    
+    tokenized = []
+    if type(word_list) == list and len(word_list) == 1:
+        word_list = word_list[0]
+
+    if type(word_list) == list:
+        word_list = ' '.join([str(elem) for elem in word_list]) 
+    # since we're only tokenizing, I remove RAM intensive operations and increase max text size
+
+    model.max_length = MAX_LEN
+    doc = model(word_list, disable=["parser", "tagger", "ner", "lemmatizer"])
+    
+    for token in doc:
+        if not token.is_punct and len(token.text.strip()) > 0:
+            tokenized.append(token.text)
+    return tokenized
+
+def normalizeTokens(word_list, extra_stop=[], model=nlp, lemma=True, MAX_LEN=1500000):
+    #We can use a generator here as we just need to iterate over it
+    normalized = []
+    if type(word_list) == list and len(word_list) == 1:
+        word_list = word_list[0]
+
+    if type(word_list) == list:
+        word_list = ' '.join([str(elem) for elem in word_list]) 
+
+    # since we're only normalizing, I remove RAM intensive operations and increase max text size
+
+    model.max_length = MAX_LEN
+    doc = model(word_list.lower(), disable=["parser", "ner"])
+
+    if len(extra_stop) > 0:
+        for stopword in extra_stop:
+            lexeme = nlp.vocab[stopword]
+            lexeme.is_stop = True
+
+    # we check if we want lemmas or not earlier to avoid checking every time we loop
+    if lemma:
+        for w in doc:
+            # if it's not a stop word or punctuation mark, add it to our article
+            if w.text != '\n' and not w.is_stop and not w.is_punct and not w.like_num and len(w.text.strip()) > 0:
+            # we add the lematized version of the word
+                normalized.append(str(w.lemma_))
+    else:
+        for w in doc:
+            # if it's not a stop word or punctuation mark, add it to our article
+            if w.text != '\n' and not w.is_stop and not w.is_punct and not w.like_num and len(w.text.strip()) > 0:
+            # we add the lematized version of the word
+                normalized.append(str(w.text.strip()))
+
+    return normalized
+
+def sent_tokenize(word_list, model=nlp):
+    doc = model(word_list)
+    sentences = [sent.text.strip() for sent in doc.sents]
+    return sentences
+
+d_env = dd.from_pandas(env, npartitions=effective_n_jobs(-1))
+env['tokenized_sents'] = env.text.apply(lambda x: [word_tokenize(s) for s in sent_tokenize(x)])
+env['normalized_sents'] = env['tokenized_sents'].apply(lambda x: [normalizeTokens(s, lemma=False) for s in x])
+env = d_env.compute()
+
+env.to_pickle('../Data/'+path+'/env_processed_sent.pkl')
+env.to_csv('../Data/'+path+'/env_processed_sent.csv')
