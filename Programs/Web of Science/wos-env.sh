2e3be9a7e559fc87e16de9106bcbd6041d22bf02
mcpackard415 <mcpackard@uchicago.edu>
2022-04-20 13:49:10 -0500

    updates

--- Programs/Web of Science/wos-env.sh
+++ Programs/Web of Science/wos-env.sh
@@ -3,8 +3,8 @@
 mysql --ssl-ca=../wos-db.uchicago.edu.ca \
       --ssl-key=../wos-db.uchicago.edu-key.pem \
       --ssl-cert=../wos-db.uchicago.edu-cert.pem \
-      -h wos-db.uchicago.edu --user=mcpackard wos_new < wos.sql > wos.tab
+      -h wos-db.uchicago.edu --user=mcpackard wos_new < wos-env.sql > wos-env.tab
 
-git add wos.tab
+git add wos-env.tab
 git commit -m 'New wos data pull'
 git push
